e57204e39898b12f2dab55871754a7e15b94bf7c
Brandon Levine <levinebr@umich.edu>
2016-11-21 22:49:21 -0500

    Initial commit.

---
+++ main.py
@@ -0,0 +1,16 @@
+from pygame import *
+from pygame.sprite import *
+from random import *
+
+screen_color = (0, 0, 0)
+
+# Brick class here
+
+
+# Ball class here
+
+
+# Paddle class here
+
+
+# Game loop here
